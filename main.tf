a96576b52781aef8a1d7993dc64cdd377794ddee
Ubuntu <azureuser@stark-JenkinsVM.pljzyan3icmexjo21uju1jsfwh.bx.internal.cloudapp.net>
2026-01-14 04:00:15 +0000

    Initial Terraform code for Jenkins CI/CD

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "stark-TerraformRG"
+  location = "East US"
+}
+
